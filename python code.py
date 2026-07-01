afe2522b420d4b2bdc8c954475ec6d54c67606f3
Navya00301 <navyamaria2005@gmail.com>
2026-07-01 15:44:52 +0530

    Add files via upload

---
+++ python code.py
@@ -0,0 +1,12 @@
+def add_numbers(a, b):
+    return a + b
+
+# Input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Function call
+result = add_numbers(num1, num2)
+
+# Output
+print("Sum =", result)
